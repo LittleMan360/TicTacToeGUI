3140b8ae30a680c79e128e4f2cb57f51647e6df5
dalto <dalton.m.towe@gmail.com>
2024-02-14 21:25:02 -0500

    fixed red text accidentally only made one column red

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -149,7 +149,7 @@ public class TicTacToeFrame
         {
             for (int row = 0; row < 3; row++)
             {
-                for (int col = 0; col < 3; row++)
+                for (int col = 0; col < 3; col++)
                 {
                     setTie(board[row][col]);
                 }
